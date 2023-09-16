491b515c21b0db4c2a3409bcc3f62674dbc322b7
Pablo Belay <pbelay@gmail.com>
2023-09-16 11:25:32 +0200

    planif mensual

--- unidadex_01/planning02/script.py
+++ unidadex_01/planning02/script.py
@@ -7,26 +7,25 @@ class PDF(FPDF):
         self.cell(0, 10, self.mes_ano, 0, new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='C')
 
     def footer(self):
-        self.set_y(-15)
+        self.set_y(-30)
         self.set_font('Helvetica', 'I', 8)
         self.cell(0, 10, 'Páxina ' + str(self.page_no()), 0, new_x=XPos.RIGHT, new_y=YPos.TOP, align='C')
 
 def xerar_planificador(pdf, mes, ano):
-    # Nomes dos meses en galego
+    # Nomes dos meses en galego (RAG)
     meses_galego = ["", "Xaneiro", "Febreiro", "Marzo", "Abril", "Maio", "Xuño", "Xullo", "Agosto", "Setembro", "Outubro", "Novembro", "Decembro"]
-    
-    pdf.mes_ano = f"{meses_galego[mes]} {ano}"  # Usar o nome do mes en galego
+    pdf.mes_ano = f"{meses_galego[mes]} {ano}"  # Definir o atributo 'mes_ano' para o encabezado
     pdf.add_page()
-    pdf.set_auto_page_break(auto=True, margin=15)
+    pdf.set_auto_page_break(auto=True, margin=35)
     pdf.set_font('Helvetica', size=12)
 
     dias_mes = calendar.monthrange(ano, mes)[1]  # Obter o número de días do mes
 
     cell_width = 210 / 7  # Ancho da páxina A4 dividido por 7 días
     header_height = 10  # Altura axustada ao texto para os días da semana
-    cell_height = (297 - 50 - header_height) / 6
+    cell_height = (297 - 65 - header_height) / 6
 
-    # Encabezado dos días
+    # Encabezado dos días en galego (RAG)
     dias = ["Luns", "Martes", "Mércores", "Xoves", "Venres", "Sábado", "Domingo"]
     for dia in dias:
         pdf.cell(cell_width, header_height, dia, 1)
