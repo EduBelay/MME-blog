ad59b9100e5443d023d4ec9b742acd585009fbc0
Pablo Belay <pbelay@gmail.com>
2024-01-08 19:32:21 +0100

    openai

---
+++ unidade06/099-gpt/01.py
@@ -0,0 +1,8 @@
+from openai import OpenAI
+
+client = OpenAI()
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+client = OpenAI(
+   api_key=os.environ.get("OPENAI_API_KEY"),
+)
